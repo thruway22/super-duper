777f6763044fd6cc6060112dcd5a14d775b3c9dd
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-05-27 06:44:29 +0300

    u

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import utilities as utl
 
-
-
 st.title('Saudi REITs')
 ticker = st.selectbox('Choose fund', utl.tickers.keys(),
                       label_visibility='collapsed', format_func=lambda x:tickers[x])
