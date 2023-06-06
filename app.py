ecb869251478e3118fb2bca46b4cbf3b8b854a0a
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-07 02:48:08 +0300

    u

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title('Saudi REITs')
 ticker = st.selectbox('Choose fund', utl.tickers.keys(),
                       label_visibility='collapsed', format_func=lambda x:utl.tickers[x])
 
-st.table(get_ticker_timeseries(ticker))
-st.table(get_ticker_categorical(ticker, 'full-year'))
+st.table(utl.get_ticker_timeseries(ticker))
+st.table(utl.get_ticker_categorical(ticker, 'full-year'))
