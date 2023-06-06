b35b7d1fc740ebd3e2494a1563330e27fcecd99a
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-07 02:49:30 +0300

    u

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ ticker = st.selectbox('Choose fund', utl.tickers.keys(),
                       label_visibility='collapsed', format_func=lambda x:utl.tickers[x])
 
 if ticker != 9999:
-    st.table(utl.get_ticker_timeseries(ticker))
+    st.table(utl.get_ticker_timeseries(ticker).head())
     st.table(utl.get_ticker_categorical(ticker, 'full-year'))
