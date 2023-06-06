de79ae99a30999332e4b5b6883eefd095ed42ed0
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-07 02:57:03 +0300

    u

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import utilities as utl
+import plotly.express as px
 
 st.title('Saudi REITs')
 ticker = st.selectbox('Choose fund', utl.tickers.keys(),
                       label_visibility='collapsed', format_func=lambda x:utl.tickers[x])
 
 if ticker != 9999:
-    st.table(utl.get_ticker_timeseries(ticker).head())
-    st.table(utl.get_ticker_categorical(ticker, 'full-year'))
+    ts = utl.get_ticker_timeseries(ticker).head()
+    ct = utl.get_ticker_categorical(ticker, 'full-year')
+
+    fig = px.line(ts, x=df.index, y=['price', 'nav'])
+    st.plotly_chart(fig)
