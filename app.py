48962351a73d117006e5f51838b6ae7e293351af
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-07 02:58:03 +0300

    u

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ ticker = st.selectbox('Choose fund', utl.tickers.keys(),
                       label_visibility='collapsed', format_func=lambda x:utl.tickers[x])
 
 if ticker != 9999:
-    ts = utl.get_ticker_timeseries(ticker).head()
+    ts = utl.get_ticker_timeseries(ticker)
     ct = utl.get_ticker_categorical(ticker, 'full-year')
-    st.table(ts)
+    st.table(ts.head())
 
     fig = px.line(ts, x=ts.index, y=['price', 'nav'])
     st.plotly_chart(fig)
