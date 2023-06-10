1a6ff8775c43bed6c354cec233a1efd86723a54d
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-11 01:22:48 +0300

    u

--- charts.py
+++ charts.py
@@ -7,7 +7,7 @@ def display_timeseries_chart(df, metric):
     pass
 
 def display_categorical_chart(df, metric):
-    fig = px.bar(df, x=df.index, y=metric, height=250)
+    fig = px.bar(df, x=df.index, y=metric, height=350)
     fig.update_layout(bargap=0.05)
     fig.update_yaxes(visible=False, showticklabels=False, showgrid=False)
     # for i, t in enumerate(texts):
