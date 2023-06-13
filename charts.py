e683234181252f81d7feb04f6557f8271ca1cf61
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-14 02:03:15 +0300

    u

--- charts.py
+++ charts.py
@@ -34,22 +34,40 @@ def display_timeseries_chart(ticker_df, sector_df, metric):
     plt.rcParams['xtick.color'] = '85868B'
 
     if metric == 'price':
-        fig, ax = plt.subplots(figsize=(6.4, 2.0))
-        ax.plot(ticker_df['price'], linewidth=1, color='lightgrey', alpha=1, label='Price')
-        ax.plot(ticker_df['nav'], linewidth=1, color='#0068c9', alpha=1, label='NAV')
-
+        xy = ticker_df['navpd']
     else:
-        fig, ax = plt.subplots(figsize=(6.4, 1.2))
         xy = ((ticker_df[metric] / sector_df[metric]) - 1) * 100
-        x_curve = xy.index
-        y_curve = xy.values
 
-        ax.plot(ticker_df[metric], color='#0068c9', linewidth=0)
+    x_curve = xy.index
+    y_curve = xy.values 
+
+    fig, ax = plt.subplots(figsize=(6.4, 1.2))
+
+    ax.plot(ticker_df[metric], linewidth=0)
+
+    ax.fill_between(x_curve, y_curve,
+                    where=(y_curve > 0), color=color_dict['more_than_zero'][metric], alpha=0.15)
+    ax.fill_between(x_curve, y_curve,
+                    where=(y_curve < 0), color=color_dict['less_than_zero'][metric], alpha=0.15)
+
+
+    # if metric == 'price':
+    #     fig, ax = plt.subplots(figsize=(6.4, 2.0))
+    #     ax.plot(ticker_df['price'], linewidth=1, color='lightgrey', alpha=1, label='Price')
+    #     ax.plot(ticker_df['nav'], linewidth=1, color='#0068c9', alpha=1, label='NAV')
+
+    # else:
+    #     fig, ax = plt.subplots(figsize=(6.4, 1.2))
+    #     xy = ((ticker_df[metric] / sector_df[metric]) - 1) * 100
+    #     x_curve = xy.index
+    #     y_curve = xy.values
+
+    #     ax.plot(ticker_df[metric], color='#0068c9', linewidth=0)
     
-        ax.fill_between(x_curve, y_curve,
-                        where=(y_curve > 0), color=color_dict['more_than_zero'][metric], alpha=0.15)
-        ax.fill_between(x_curve, y_curve,
-                        where=(y_curve < 0), color=color_dict['less_than_zero'][metric], alpha=0.15)
+    #     ax.fill_between(x_curve, y_curve,
+    #                     where=(y_curve > 0), color=color_dict['more_than_zero'][metric], alpha=0.15)
+    #     ax.fill_between(x_curve, y_curve,
+    #                     where=(y_curve < 0), color=color_dict['less_than_zero'][metric], alpha=0.15)
 
         # ax.plot(ticker_df[metric], linewidth=1, color='#0068c9', alpha=1, label='Ticker')
         # ax.plot(sector_df[metric], linewidth=1, color='lightgrey', alpha=1, label='Sector')
