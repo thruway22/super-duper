baa06db783274653e16e2940c0e4f558db11c3e6
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-14 01:24:10 +0300

    u

--- charts.py
+++ charts.py
@@ -44,12 +44,12 @@ def display_timeseries_chart(ticker_df, sector_df, metric):
         x_curve = xy.index
         y_curve = xy.values
 
-        ax.plot(ticker_df[metric_col], color='#0068c9', linewidth=0)
+        ax.plot(ticker_df[metric], color='#0068c9', linewidth=0)
     
         ax.fill_between(x_curve, y_curve,
-                        where=(y_curve > 0), color=color_dict['more_than_zero'][metric_col], alpha=0.15)
+                        where=(y_curve > 0), color=color_dict['more_than_zero'][metric], alpha=0.15)
         ax.fill_between(x_curve, y_curve,
-                        where=(y_curve < 0), color=color_dict['less_than_zero'][metric_col], alpha=0.15)
+                        where=(y_curve < 0), color=color_dict['less_than_zero'][metric], alpha=0.15)
 
         # ax.plot(ticker_df[metric], linewidth=1, color='#0068c9', alpha=1, label='Ticker')
         # ax.plot(sector_df[metric], linewidth=1, color='lightgrey', alpha=1, label='Sector')
