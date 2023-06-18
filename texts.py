a27ad034ba458a7343adb7f153c4496826c250d8
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-06-19 01:00:25 +0300

    u

--- texts.py
+++ texts.py
@@ -20,7 +20,7 @@ bodies = {
     'pffo': "This chart tracks daily movements in a fund's Price to Funds From Operations (P/FFO) ratio, against the normalized sector's median P/FFO. Values above zero suggest higher valuation compared to peers, while those below indicate lower valuation. This metric enables comparison of a REIT's valuation to the sector median, offering a measure of relative profitability and potential investment value.",
     'ffos': "It refers to the portion of Funds From Operations (FFO) allocated to each share. This metric is an insightful tool for assessing a REIT's profitability per share.",
     'ffo_payout': "It denotes the percentage of FFO distributed as dividends. A lower payout ratio may suggest a REIT's potential for more substantial business growth.",
-    'ffo_margin': "This is the percentage of total revenue that remains after all operating expenses have been paid. Higher FFO Margins typically signify more efficient REIT operations.",
+    'ffo_margin': "This is the percentage of total revenue that remains after all operating expenses have been paid. Higher FFO Margins signify more efficient REIT operations.",
     'ffo_roic': "It's Return on Invested Capital (ROIC) using FFO. This metric measures how efficiently a REIT uses its capital to generate FFO.",
     'ffo_coverage': "This ratio assesses a REIT's ability to cover its debt using its FFO. A higher FFO Coverage Ratio typically indicates healthier financial strength.",
     'ffo_leverage': "It's the level of a REIT's debt compared to its FFO. A lower FFO Leverage Ratio usually means greater financial stability for the REIT."
